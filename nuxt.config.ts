43fdbba483678e5285746f5fc4ad5753925e3cfd
Safeeullah Sarhandi <sarhandi4@gmail.com>
2022-10-25 13:18:03 +0500

    changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    ssr : false,
     css : [
         'vue-toastification/dist/index.css',
         'vue3-tel-input/dist/vue3-tel-input.css',
